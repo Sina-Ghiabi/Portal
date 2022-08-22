2a3d8297115f05c97eb0a311575c25cfd87f6e48
Sina Ghiabi <110805470+Sina-Ghiabi@users.noreply.github.com>
2022-08-22 09:20:35 +0430

    Add files via upload

---
+++ RemainCreditDto.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Dapna.MSVPortal.Financial.Dto
+{
+    [AutoMap(typeof(RemainCredit))]
+    public class RemainCreditDto : FullAuditedEntityDto<int>
+    {
+        [Required]
+        public int ProjectID { get; set; }
+        [Required]
+        public string ProjectCode { get; set; }
+        [Required]
+        public string ProjectName { get; set; }
+        [Required]
+        public string FourthLevelCode { get; set; }
+        [Required]
+        public string PayTo { get; set; }
+        public long? RemainDebtAmount { get; set; }
+        public long? RemainCreditAmount { get; set; }
+    }
+}

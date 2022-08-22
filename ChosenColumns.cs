2ff0436d30177b5e678f6c65e747f91b9df63cec
Sina Ghiabi <110805470+Sina-Ghiabi@users.noreply.github.com>
2022-08-22 09:40:08 +0430

    Add files via upload

---
+++ ChosenColumns.cs
@@ -0,0 +1,52 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Dapna.MSVPortal.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Dapna.MSVPortal.Projects
+{
+    public class ChosenColumns : FullAuditedEntity<int>
+    {
+        public int UserID { get; set; }
+        public string Section { get; set; }
+        public int TableID { get; set; }
+        public bool Column1 { get; set; }
+        public bool Column2 { get; set; }
+        public bool Column3 { get; set; }
+        public bool Column4 { get; set; }
+        public bool Column5 { get; set; }
+        public bool Column6 { get; set; }
+        public bool Column7 { get; set; }
+        public bool Column8 { get; set; }
+        public bool Column9 { get; set; }
+        public bool Column10 { get; set; }
+        public bool Column11 { get; set; }
+        public bool Column12 { get; set; }
+        public bool Column13 { get; set; }
+        public bool Column14 { get; set; }
+        public bool Column15 { get; set; }
+        public bool Column16 { get; set; }
+        public bool Column17 { get; set; }
+        public bool Column18 { get; set; }
+        public bool Column19 { get; set; }
+        public bool Column20 { get; set; }
+        public bool Column21 { get; set; }
+        public bool Column22 { get; set; }
+        public bool Column23 { get; set; }
+        public bool Column24 { get; set; }
+        public bool Column25 { get; set; }
+        public bool Column26 { get; set; }
+        public bool Column27 { get; set; }
+        public bool Column28 { get; set; }
+        public bool Column29 { get; set; }
+        public bool Column30 { get; set; }
+        public bool Column31 { get; set; }
+        public bool Column32 { get; set; }
+        public bool Column33 { get; set; }
+        public bool Column34 { get; set; }
+        public bool Column35 { get; set; }
+    }
+}

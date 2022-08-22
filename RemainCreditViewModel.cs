9027341f4b345fda063dc5944f8de716ed4bf5a0
Sina Ghiabi <110805470+Sina-Ghiabi@users.noreply.github.com>
2022-08-22 09:16:14 +0430

    Add files via upload

---
+++ RemainCreditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapna.MSVPortal.Web.ViewModels
+{
+    public class RemainCreditViewModel
+    {
+        [Required]
+        public int ProjectID { get; set; }
+        [Required]
+        public string ProjectCode { get; set; }
+        [Required]
+        public string ProjectName { get; set; }
+        [Required]
+        public string FourthLevelCode { get; set; }
+        [Required]
+        public string PayTo { get; set; }
+        public string RemainDebtAmount { get;set;}
+        public string RemainCreditAmount { get; set; }
+        public string UploadDate { get; set; }
+        public string TotalDebtAmount { get; set; }
+        public string TotalCreditAmount { get; set; }
+
+    }
+}

592f2d618d5bd85670d48ca1878a35816dcfe81b
Sina Ghiabi <110805470+Sina-Ghiabi@users.noreply.github.com>
2022-08-22 09:18:42 +0430

    Add files via upload

---
+++ RemainCredit.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Dapna.MSVPortal.Financial
+{
+     public class RemainCredit : FullAuditedEntity<int>
+    {
+        [Required]
+        public int ProjectID { get; set; }
+        [Required]
+        public string ProjectCode { get; set; }
+        [Required]
+        public string ProjectName { get; set; }
+        [Required]
+        public string FourthLevelCode { get; set; }
+        [Required]
+        public string PayTo { get; set; }
+        public long? RemainDebtAmount { get; set; }
+        public long? RemainCreditAmount { get; set; }
+    }
+}
